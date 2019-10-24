32b46dcef91f554cb9a35b443f3016205b1cc828
Ivan Dai <ivandaixivwork@gmail.com>
2019-10-24 11:55:22 -0700

    Home section from NYT implementation: styling needed

--- js/script.js
+++ js/script.js
@@ -11,15 +11,23 @@ $(function(){
         url: 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=vRtDeuRkRsAJWXB8I6P5L5cUg3dvnD99'
     })
     .done(function(data){
-        console.log(data);
-        $.each(data, function(){
-            $('.articles').append(this.status);
+        // console.log(data);
+        $('.articles').append('<div class="currentNews"></div>');
+        let articleNumber = 1;
+        $.each(data.results, function(){
+            console.log(this);
+            console.log(this.multimedia[4].url);
+            $('.currentNews').append('<a class="article-link" href="' + this.url +'"><div class="article-box article' + articleNumber + '"> <p class="article-text">' + this.abstract + '</p></div></a>');
+            $('.article' + articleNumber).css('background-image', 'url(' + this.multimedia[4].url.replace("\"","")  +')');
+            articleNumber++;
         })
     })
     .fail(function(){
         $('.articles').append('Sorry there was an error.')
     })
-    .always(function(){
-
-    })
+    // .always(function(){
+    //     let today = new Date();
+    //     let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    //     $('.articles').append('<p>Todays Date: ' + date + '</p>')
+    // })
 });
