445b5b1a440304b8523976a8f37dd1e604d486a3
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-03 19:25:17 -0800

    README.md added

---
+++ README.md
@@ -0,0 +1,25 @@
+#InstaNews Project
+
+InstaNews Project, NYT API News site
+
+##Key Learning
+
+HTML, CSS, Javascript, Gulp, Sass, API's, Minify, Json, User's Attention, Accerssibility, etc...
+
+##Technologies Used
+
+New York Times Top Stories API, JQuery
+
+##Personal Experience
+
+Assigning classes with javascript is better than assigning individual styles to the newly created items (More Efficient). Flexbox and Grid should not be used together because that could possibily cause conflicts.
+
+##Website Description
+
+This website displays articles from a chosen list of cateogries available to the user to chose. It uses the New York Times Top Stories API to load in the newest articles from the respective categories.
+
+##Credit
+    1. https://jquery.com
+    2. https://developer.nytimes.com
+    3. https://sass-lang.com
+    
--- js/script.js
+++ js/script.js
@@ -26,41 +26,45 @@ $(function(){
             $('.articles').append('<div class="currentNews" id="currentNews"></div>');
             let articleCount = 1;
             // let gridCounter = 1;
-            $.each(data.results, function(){
-                // index or counter
-                // console.log(this);
-                // console.log(this.multimedia[4].url);
-                if(this.multimedia.length >= 4){
-                    if(articleCount <= 12) {
-                        $('.currentNews').append('<a class="article-link" href="' + this.url +'"><div class="article-box article' + articleCount + '"> <p class="article-text">' + this.abstract + '</p></div></a>');
-                        // console.log(index, this);
-                        $('#currentNews').css('display','grid');
-                        // APPLYS A NEW GRID ON EVERY ELEMENT
-                        $('.article' + articleCount).css('background-image', 'url(' + this.multimedia[4].url  +')');
-                        $('body').css({'height' : '100%' , });
-                        $('.nyt-logo').css('height','120px');
-                        $('header').css({'justify-content' : 'flex-start', 'width' : '100%'}); 
-                        articleCount++;  
-                        // if(gridCounter == 1 ){
-                        //     $('.article' + articleCount).css('grid-column', '1/2');
-                        //     gridCounter++;
-                        // } else {
-                        //     $('.article' + articleCount).css('grid-column', '3/4');
-                        //     gridCounter = 1;
-                        // }
-                        // if(gridCounter == 1 ){
-                        //     $('.article' + articleCount).css('grid-column', '1/2');
-                        //     gridCounter++;
-                        // } else if( gridCounter == 2) {
-                        //     $('.article' + articleCount).css('grid-column', '2/3');
-                        //     gridCounter++;
-                        // } else {
-                        //     $('.article' + articleCount).css('grid-column', '3/4');
-                        //     gridCounter = 1;
-                        // }
+            $('#loader').css('display', 'flex');
+            setTimeout(function(){
+                $('#loader').css('display', 'none');
+                $.each(data.results, function(){
+                    // index or counter
+                    // console.log(this);
+                    // console.log(this.multimedia[4].url);
+                    if(this.multimedia.length >= 4){
+                        if(articleCount <= 12) {
+                            $('.currentNews').append('<a class="article-link" href="' + this.url +'"><div class="article-box article' + articleCount + '"> <p class="article-text">' + this.abstract + '</p></div></a>');
+                            // console.log(index, this);
+                            $('#currentNews').css('display','grid');
+                            // APPLYS A NEW GRID ON EVERY ELEMENT
+                            $('.article' + articleCount).css('background-image', 'url(' + this.multimedia[4].url  +')');
+                            $('body').css({'height' : '100%' , });
+                            $('.nyt-logo').css('height','120px');
+                            $('header').css({'justify-content' : 'flex-start', 'width' : '100%'}); 
+                            articleCount++;  
+                            // if(gridCounter == 1 ){
+                            //     $('.article' + articleCount).css('grid-column', '1/2');
+                            //     gridCounter++;
+                            // } else {
+                            //     $('.article' + articleCount).css('grid-column', '3/4');
+                            //     gridCounter = 1;
+                            // }
+                            // if(gridCounter == 1 ){
+                            //     $('.article' + articleCount).css('grid-column', '1/2');
+                            //     gridCounter++;
+                            // } else if( gridCounter == 2) {
+                            //     $('.article' + articleCount).css('grid-column', '2/3');
+                            //     gridCounter++;
+                            // } else {
+                            //     $('.article' + articleCount).css('grid-column', '3/4');
+                            //     gridCounter = 1;
+                            // }
+                        }
                     }
-                }
-            })
+                })
+            }, 1000);
         })
         .fail(function(){
             $('.articles').append('<p id="error">Sorry there was an error. Did you choose properly?</p>')
